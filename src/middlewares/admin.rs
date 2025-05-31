4090684863fccf57dc361dcc095f917c5cfbc07e
Vishnu Kumar <vishnukumart96@gmail.com>
2025-05-31 09:46:03 +0530

    feat: add admin middleware

---
+++ src/middlewares/admin.rs
@@ -0,0 +1,42 @@
+use actix_web::{body::MessageBody, dev::{ServiceRequest, ServiceResponse}, middleware::Next, Error, HttpMessage};
+use jsonwebtoken::{decode, DecodingKey, Validation};
+
+use crate::{errors::CustomError, schema::JWTClaims};
+
+pub async fn admin_middleware(
+    req:ServiceRequest,
+    next: Next<impl MessageBody>
+) -> Result<ServiceResponse<impl MessageBody>, Error>{
+
+    let auth_header = req.headers().get("Authorization");
+
+    if auth_header.is_none(){
+        return  Err(Error::from(CustomError{error:"token missing"}));
+    }
+
+    let token = auth_header.unwrap().to_str();
+
+    if token.is_err(){
+        return Err(Error::from(CustomError{error:"Internal Server Error"}));
+    }
+
+    let token = token.unwrap();
+
+    let jwt_secret = std::env::var("ADMIN_JWT_PASSWORD").unwrap();
+
+    let decoded = decode::<JWTClaims>(token,
+         &DecodingKey::from_secret(jwt_secret.as_bytes()), &Validation::default());
+
+    if decoded.is_err(){
+        println!("error while decoding admin jwt key");
+        return Err(Error::from(CustomError{error:"Internal Error"}));
+    }
+
+    let admin_email = decoded.unwrap().claims.sub;
+
+    req.extensions_mut().insert(admin_email);
+
+    next.call(req).await
+
+
+}
